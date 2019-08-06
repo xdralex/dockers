2358e432115fa94f31acbefbb35123e7fac921a7
Alexander Demidko <alexander@fluence.one>
2019-08-06 14:23:09 +0300

    Mounts

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM continuumio/anaconda3:2019.07
 
 RUN useradd --create-home --shell /bin/bash apollo
-RUN echo "\nPATH=$PATH:/opt/conda/bin/\n" >> /home/apollo/.profile
 
 USER apollo
 WORKDIR /home/apollo
+
+ENV PATH="${PATH}:/opt/conda/bin/"
 
 EXPOSE 8888
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-IMAGE=${1:-coreml:latest}
-
-PORT=4004  # Jupyter port
-
-exec docker run --rm -u apollo -p $PORT:8888 -v "$PWD/data:/home/apollo/data" jupyter-notebook --NotebookApp.ip=0.0.0.0 --NotebookApp.password_required=False --NotebookApp.token='' --NotebookApp.custom_display_url="http://localhost:$PORT"
